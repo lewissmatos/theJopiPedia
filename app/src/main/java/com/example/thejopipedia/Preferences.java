ba7470f3cc1d1d6d96e3146d40d0604988255fd4
Lewis Matos <lewiismb1@gmail.com>
2020-04-12 16:06:24 -0400

    rediseñando la interfaz 2.0. todos los bugs resueltos; inicio de la indroducción de la base de datos

--- app/src/main/java/com/example/thejopipedia/Preferences.java
+++ app/src/main/java/com/example/thejopipedia/Preferences.java
@@ -1,5 +1,6 @@
 package com.example.thejopipedia;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 
 public class Preferences {
@@ -9,7 +10,15 @@ public class Preferences {
     public String correo;
     public String contraseña;
 
-    public static void SaveUserData(int id, String name, String email, String pass){
-        SharedPreferences preferences;
+    public static void SaveUserData(Context context, int id, String name, String email, String pass){
+        SharedPreferences preferences = context.getSharedPreferences("datos", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor=preferences.edit();
+        editor.putInt("id", id);
+        editor.putString("nombre", name);
+        editor.putString("correo", email);
+        editor.putString("contraseña", pass);
+
+        editor.commit();
     }
 }
