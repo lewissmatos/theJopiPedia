3f0bed4b8a65853a230b3649c0ccc64bebb36461
Lewis Matos <lewiismb1@gmail.com>
2020-04-13 11:58:18 -0400

    rediseñando la interfaz 2.1.5 todos los bugs de diseño resueltos;

---
+++ app/src/main/java/com/example/thejopipedia/Notas.java
@@ -0,0 +1,16 @@
+package com.example.thejopipedia;
+
+public class Notas {
+
+    private String encabezado;
+    private String apunte;
+
+    public Notas(String encabezado, String apunte) {
+        this.encabezado = encabezado;
+        this.apunte = apunte;
+    }
+
+    public Notas() {
+
+    }
+}
--- app/src/main/java/com/example/thejopipedia/Perfil_Apuntes_Fragment.java
+++ app/src/main/java/com/example/thejopipedia/Perfil_Apuntes_Fragment.java
@@ -1,22 +1,43 @@
 package com.example.thejopipedia;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
 
 
-public class Perfil_Apuntes_Fragment extends Fragment {
+public class Perfil_Apuntes_Fragment extends Fragment implements View.OnClickListener {
 
+    private ImageView añadir_nota;
+    private TextView txt_añadir;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_perfil__apuntes_, container, false);
 
+        añadir_nota = view.findViewById(R.id.añadir_nota);
+        txt_añadir = view.findViewById(R.id.txt_añadir);
+
+        añadir_nota.setOnClickListener(this);
+        txt_añadir.setOnClickListener(this);
+
+
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.añadir_nota:
+            case R.id.txt_añadir:
+
+
+        }
     }
 }
